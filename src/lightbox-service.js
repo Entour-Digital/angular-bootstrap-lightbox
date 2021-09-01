b47788e16f20aa42e9d009a10e11a741b0a3bb8b
Matthieu Nelmes <matthieunelmes@gmail.com>
2021-09-01 15:39:23 +0100

    Fix modal close exception

--- src/lightbox-service.js
+++ src/lightbox-service.js
@@ -267,7 +267,7 @@ angular.module('bootstrapLightbox').provider('Lightbox', function () {
       }, modalParams || {}));
 
       // modal close handler
-      Lightbox.modalInstance.result['finally'](function () {
+      Lightbox.modalInstance.result.finally(function () {
         // prevent the lightbox from flickering from the old image when it gets
         // opened again
         Lightbox.images = [];
@@ -282,7 +282,7 @@ angular.module('bootstrapLightbox').provider('Lightbox', function () {
         if (cfpLoadingBar) {
           cfpLoadingBar.complete();
         }
-      });
+      }).then(function() {}, function() {});
 
       return Lightbox.modalInstance;
     };
